0460f5990e1e2a643924e5292632d7bb82bf6f03
Niamh <niamh.linguist@gmail.com>
2024-08-06 12:26:36 +0300

    sidebar

---
+++ components/Logo.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="overflow-hidden rounded-lg w-[45px] h-[45px]">
+    <img src="@/assets/imgs/logo.png" alt="logo" />
+  </div>
+</template>
---
+++ components/Sidebar/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const loading = ref(false);
+</script>
+
+<template>
+  <div><Logo /></div>
+</template>
