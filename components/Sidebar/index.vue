e739e45a5f0816fde1a6d71c5c1dc2eff5946d49
Niamh <niamh.linguist@gmail.com>
2024-08-06 13:02:46 +0300

    mobile menu

--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,10 +1,37 @@
 <script setup>
-const loading = ref(false);
+const open = ref(true);
 </script>
 
 <template>
-  <div class="relative">
-    <div class="flex lg:hidden"><SidebarMenu /></div>
-    <div class="hidden lg:flex"><SidebarMenu /></div>
+  <div>
+    <div
+      class="flex lg:hidden z-50 items-center justify-between w-full h-full p-4"
+    >
+      <Logo />
+      <Icon
+        class="z-50 cursor-pointer left-2 top-2"
+        size="30"
+        name="iconamoon:menu-burger-horizontal"
+        @click="open = true"
+      />
+      <div
+        v-if="open"
+        class="fixed top-0 left-0 w-full h-screen bg-white z-[999]"
+      >
+        <Icon
+          class="absolute z-50 cursor-pointer right-4 top-4"
+          size="30"
+          name="material-symbols:close"
+          @click="open = false"
+        />
+        <SidebarMenu />
+      </div>
+    </div>
+    <div
+      class="hidden lg:flex w-[250px] h-screen flex-col justify-between border-r"
+    >
+      <SidebarMenu />
+      <div>UserItem</div>
+    </div>
   </div>
 </template>
